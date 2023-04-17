16b6d471bf927c600956f0633b965e8679deefc9
Kenta Kubo <601636+kkk669@users.noreply.github.com>
2023-04-18 02:12:38 +0900

    Format code

--- MyFirstDriver/MyFirstDriver.cpp
+++ MyFirstDriver/MyFirstDriver.cpp
@@ -7,16 +7,11 @@
 
 #include <os/log.h>
 
-#include <DriverKit/IOUserServer.h>
-#include <DriverKit/IOLib.h>
-
 #include "MyFirstDriver.h"
 
-kern_return_t
-IMPL(MyFirstDriver, Start)
+kern_return_t IMPL(MyFirstDriver, Start)
 {
-    kern_return_t ret;
-    ret = Start(provider, SUPERDISPATCH);
+    kern_return_t ret = Start(provider, SUPERDISPATCH);
     os_log(OS_LOG_DEFAULT, "Hello World");
     return ret;
 }
